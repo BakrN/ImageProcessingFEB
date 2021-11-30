c80bf0f0dfb8d4be942406b4a8da025617c29b3b
Abubakr Nada <abubakr_ehab@hotmail.com>
2021-11-30 14:46:07 +0100

    Working interface with device

--- src/DualCamera.h
+++ src/DualCamera.h
@@ -1,10 +1,30 @@
+#pragma once 
 #include "camera/DxImageProc.h"
 #include "camera/GxIAPI.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
 class DualCamera{
     private:
-    public: 
-        DualCamera(); 
-        ~DualCamera(); 
-   
+        cv::Mat m_Frames[2]; 
+        cv::Mat m_InternalParams[2]; 
+        int m_DistortionCoeff[2][5]; // 1st row - x 2nd row - y 
 
+    public: 
+        DualCamera() = default; 
+        DualCamera(int width, int height); 
+        ~DualCamera() = default;  
+        void SetImage(const cv::Mat& DualImage) {
+ 
+              m_Frames[0] = DualImage(cv::Range(0, DualImage.rows - 1), cv::Range(0, (DualImage.cols/2) - 1)); // 
+              m_Frames[1] = DualImage(cv::Range(0, DualImage.rows - 1), cv::Range(DualImage.cols/2, DualImage.cols- 1));
+        }
+        void SetImage(const cv::Mat& DualImage, int cv_conversion_code) {
+              m_Frames[0] = DualImage(cv::Range(0, DualImage.rows - 1), cv::Range(0, (DualImage.cols/2) - 1));
+              m_Frames[1] = DualImage(cv::Range(0, DualImage.rows - 1), cv::Range(DualImage.cols/2, DualImage.cols- 1));
+              cv::cvtColor(m_Frames[0], m_Frames[0], cv_conversion_code); 
+              cv::cvtColor(m_Frames[1], m_Frames[1], cv_conversion_code); 
+        }
+        cv::Mat& GetLeft(){return m_Frames[0]; }
+        cv::Mat& GetRight(){return m_Frames[1]; }
 }; 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,66 @@
 
-#include <ros/ros.h> 
 
-int main(){
-    return 0 ; 
+#include <ros/ros.h> 
+#include "GXDevice.h"
+#include "DualCamera.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+
+
+
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv,"vision_node"); 
+    ros::NodeHandle nh; 
+    DualCamera Camera; 
+    GXDevice VisionDevice; 
+    VisionDevice.Init(); 
+      
+
+    cv::namedWindow("LeftFeed"); 
+    cv::namedWindow("RightFeed");
+    cv::Mat Data; 
+    VisionDevice.StartStream(); 
+    while(1){
+        PGX_FRAME_BUFFER p_FrameData= VisionDevice.AquireImage();
+        if(!p_FrameData){
+            ROS_INFO("NULL Exit"); 
+            break; 
+        }
+        if(p_FrameData->nStatus == GX_STATUS_SUCCESS){
+       
+        //Do some image processing operations.
+         //cv::Size(p_FrameData->nHeight, p_FrameData->nWidth),CV_8UC1,(void*)(p_FrameData->pImgBuf)); 
+        Data.create(p_FrameData->nHeight,p_FrameData->nWidth,CV_8UC1); 
+        Data.data = (uchar*)p_FrameData->pImgBuf; 
+        //cv::cvtColor(Data,Data,cv::COLOR_BayerRG2GRAY); 
+        cv::flip(Data,Data,0); 
+        //imshow("LeftFeed", Data); 
+        //ROS_INFO("DATA CREATED"); 
+        //Data.data = (uchar*) p_FrameData->pImgBuf; //cpy ctr 
+        // ROS_INFO("DATA ASSIGNED"); 
+        
+        Camera.SetImage(Data, cv::COLOR_BayerRG2GRAY); 
+        cv::Mat& LeftImg =  Camera.GetLeft(); 
+        cv::Mat& RightImg =  Camera.GetRight(); 
+        cv::resize(LeftImg,LeftImg, cv::Size(LeftImg.rows/2, LeftImg.cols/2)); 
+        cv::resize(RightImg,RightImg, cv::Size(RightImg.rows/2, RightImg.cols/2)); 
+        imshow("LeftFeed", LeftImg); 
+        imshow("RightFeed", RightImg); 
+  
+        }
+        if(cv::waitKey(1) == (int)('q')){
+            break; 
+        } 
+    } 
+     ROS_INFO("STOPPED STREAM"); 
+    VisionDevice.StopStream(); 
+    cv::destroyAllWindows(); 
+
+    VisionDevice.Shutdown(); 
+    ros::shutdown(); 
+    return 0; 
 }
